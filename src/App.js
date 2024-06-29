db02c218e6bdc448e7587a65a7a23d9873ebd295
yni <yn2443@columbia.edu>
2024-06-29 16:55:55 -0400

    Dummy version of the board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  // button property or prop that tells CSS how to style the button
+  // the className is "square", which corr. to .square in the styles.css file 
+
+  return ( // parentheses used to group complex return statements to improve code readability
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">1</button>
+        <button className="square">1</button>
+      </div>
+      <div className="board-row">
+        <button className="square">2</button>
+        <button className="square">2</button>
+        <button className="square">2</button>
+      </div>
+      <div className="board-row">
+        <button className="square">3</button>
+        <button className="square">3</button>
+        <button className="square">3</button>
+      </div>
+    </> 
+  );
 }
